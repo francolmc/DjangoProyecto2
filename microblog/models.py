3c62617ac849c8013bbfb00535e502ecf86ff9bf
Franco Morales Castro <franco.morales@outlook.com>
2025-09-01 20:14:12 -0400

    feat: add tables on model for microblos

--- microblog/models.py
+++ microblog/models.py
@@ -5,3 +5,31 @@ class Categories(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+#TODO: Crear clase de users
+class Users(models.Model):
+    name = models.CharField(max_length=25, blank=False)
+    email = models.CharField(max_length=320, blank=False, unique=True)
+    password = models.CharField(max_length=25, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+#TODO: Crear clase de posts
+class Posts(models.Model):
+    content = models.CharField(max_length=140, blank=False)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+#TODO: Crear clase de likes
+class Likes(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+#TODO: Crear clase de comments
+class Comments(models.Model):
+    content = models.CharField(max_length=140, blank=False)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
