5b407fcaf0d90782b523c3ed4c22ef5d9df80eee
Franco Morales Castro <franco.morales@outlook.com>
2025-09-10 20:44:06 -0300

    feat: registro de usuario

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,3 +1,11 @@
 #imports
+from django.urls import path, include
+from . import views
+
 app_name = 'microblog'
+
 #configuracion
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register', views.CustomSignUpView.as_view(), name='register')
+]
